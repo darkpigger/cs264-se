192af20a105b7be259670cff5f5ca8811239fd87
darkpigger <firstchokchai@hotmail.com>
2019-11-19 17:59:36 +0700

    Edit port

--- main.py
+++ main.py
@@ -9,5 +9,6 @@ def home():
 	return render_template('template.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    # app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
 
