65cd9bd50cf39aeeedaaebd1e764455303cc1abb
darkpigger <firstchokchai@hotmail.com>
2019-11-20 19:57:09 +0700

    update page

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ app.config['SECRET_KEY'] = 'secret!'
 @app.route('/')
 def home():
 	return render_template('template.html')
+def login():
+    return render_login('login_student.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
     #app.run(host='0.0.0.0', port=5000)
     
-if __name__ == '__login__':
+if __name__ == '__login_student__':
     app.run(host='0.0.0.0', port=80)
     #app.run(host='0.0.0.0', port=5000)
 
